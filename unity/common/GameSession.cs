2e1c6333409dd8540d2ef0b760cfc81d2b81e218
gsg-jay <jay@gemsword.games>
2023-10-10 11:55:17 +0100

    (feat):Implement game mode

--- unity/common/GameSession.cs
+++ unity/common/GameSession.cs
@@ -6,6 +6,13 @@ namespace GSG
 {
     public class GameSession : MonoBehaviour
     {
+        public enum GAME_MODE_TYPE
+        {
+            STORY,
+            VS_BATTLE,
+            TRAINING
+        };
+        public GAME_MODE_TYPE currentGameMode;
         public List<Player> players = new List<Player>();
         public int[] scores;
         public int currentTimeElapsedInMs;
