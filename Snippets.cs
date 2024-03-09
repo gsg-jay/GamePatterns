b07445ae610165e80b40f6bba8c15c428e8e3640
gsg-jay <120525462+gsg-jay@users.noreply.github.com>
2024-03-09 17:44:31 +0000

    Update Snippets.cs

--- Snippets.cs
+++ Snippets.cs
@@ -1,35 +1,63 @@
 // Mystical Medic
 
 public class FightingAction : MonoBehaviour {
-  private PlayableCharacter playableCharacter;
-  private string commaSeparatedCancelOptions;
+  private Player _player;
+  private PlayableCharacter _playableCharacter;
+  private string _commaSeparatedCancelOptions;
+  private float _gamepadDeadZone; 
   
   void Start() {
-    playableCharacter = GetComponent<PlayableCharacter>();
+    _playableCharacter = GetComponent<PlayableCharacter>();
+    _player = _playableCharacter.GetPlayer();
+    _gamepadDeadZone = _player.GetGamepadDeadZone();
+  }
+
+  private bool CommandReceivedNormalAttack() {
+    return !_player.buttonSpecial.IsPressed() && _player.buttonAttack.IsPressed();
+  }
+  private bool CommandReceivedForwardSpecial() {
+    return _player.IsMoving() && _player.buttonSpecial.IsPressed();
+  }
+  private bool CommandReceivedDownSpecial() {
+    return _player.IsCrouching() && _player.buttonSpecial.IsPressed();
+  }
+  private bool CommandReceivedUpSpecial() {
+    return _player.leftStickY > _gamepadDeadZone && _player.buttonSpecial.IsPressed();
+  }
+  private bool CommandReceivedNeutralSpecial() {
+    return (
+      Math.Abs(_player.leftStick.y) < _gamepadDeadZone 
+      && Math.Abs(_player.leftStick.y) < _gamepadDeadZone  
+      && _player.buttonSpecial.IsPressed()
+    )
   }
   
   void Update() {
-    var attackNeutral = playableCharacter.DidReceiveAttackCommand("attackNeutral");       // ▢
-    var specialNeutral = playableCharacter.DidReceiveAttackCommand("specialNeutral");     // ○
-    var specialUp = playableCharacter.DidReceiveAttackCommand("specialUp");               // ↑ + ○ 
-    var specialDown = playableCharacter.DidReceiveAttackCommand("specialDown");           // ↓ + ○
-    var specialForward = playableCharacter.DidReceiveAttackCommand("specialForward");     // → ○
+    var attackNeutral = CommandReceivedNormalAttack();                                     // ▢
+    var specialNeutral = CommandReceivedNeutralSpecial();     // ○
+    var specialUp = CommandReceivedUpSpecial();               // ↑ + ○ 
+    var specialDown = CommandReceivedDownSpecial();           // ↓ + ○
+    var specialForward = CommandReceivedForwardSpecial();     // → ○
     
-    if (commaSeparatedCancelOptions.Includes("attackNeutral") && attackNeutral){
-      playableCharacter.Attack("normal");
+    if (commaSeparatedCancelOptions.Includes("specialUp") && specialUp){
+      playableCharacter.Attack("specialUp");
+      return;
+    }
+    if (commaSeparatedCancelOptions.Includes("specialDown") && specialDown){
+      playableCharacter.Attack("specialDown");
+      return;
+    }
+    if (commaSeparatedCancelOptions.Includes("specialForward") && specialForward){
+      playableCharacter.Attack("specialForward");
       return;
     }
     if (commaSeparatedCancelOptions.Includes("specialNeutral") && specialNeutral){
       playableCharacter.Attack("specialNeutral");
+      return;
     }
-    if (commaSeparatedCancelOptions.Includes("specialUp") && specialUp){
-      playableCharacter.Attack("specialUp");
-    }
-    if (commaSeparatedCancelOptions.Includes("specialDown") && specialDown){
-      playableCharacter.Attack("specialDown");
-    }
-    if (commaSeparatedCancelOptions.Includes("specialForward") && specialForward){
-      playableCharacter.Attack("specialForward");
+    if (commaSeparatedCancelOptions.Includes("attackNeutral") && attackNeutral){
+      playableCharacter.Attack("normal");
+      return;
     }
   }
 
