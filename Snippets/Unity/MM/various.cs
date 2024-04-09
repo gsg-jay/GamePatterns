6f286eef3a06d651430dcb29601b9b18b16cc11b
gsg-jay <120525462+gsg-jay@users.noreply.github.com>
2024-04-09 13:25:39 +0100

    Create various.cs

---
+++ Snippets/Unity/MM/various.cs
@@ -0,0 +1,75 @@
+// Events
+public class EventManager : MonoBeahaviour {
+	public static class EventKeys {
+		/* Event names */
+	}
+	public class EventPayload {
+		public struct OnEventPause{ /* ... */ }
+		public struct OnEventSetSaveFile { /* ... */ }
+		// etc...
+	}
+	public static Action<EventPayload> 
+}
+
+// System
+public class GameManager : MonoBehaviour {
+	
+	// Singleton
+	public static GameManager Instance;
+	
+	protected void Awake() {
+		if (Instance == null){
+			Instance = this;
+	}
+	
+	protected void OnEnable(){
+		
+	}
+	protected void OnDisable(){
+		
+	}
+	
+	// Game
+	public class LevelManager {
+	 public static void CompleteLevel(string levelName);
+	}
+	
+	public class IOManager {
+	 public static void Pause();
+	 public static void SetCheckpoint(string currentSavePoint);
+	 public static void SetSaveFile(string saveFileName);
+	 public static void Save(string currentSaveFileName);
+	 public static void Load(string saveFileName)
+	}
+	
+	// Screen FX
+	public class VFXManager {
+	 public void VFXScreenFreeze(/* args */);
+	 public void VFXTakeHeavyDamage(/* args */);
+	 public void VFXSuperAttack(/* args */);
+	 public void VFXBossDefeatedFX(/* args */);
+	}
+
+	// Tips + UI
+	public class TutorialManager {
+		public void FetchTip();
+		public void ShowNextTip();
+		public void HideTip();
+	}
+	
+	
+	// Game IO
+	public void HandleLoadNextLevel(string levelName);
+	public void 
+	
+} 
+
+	public void HandleGameOver();
+public class PlayableCharacter : MonoBehaviour;
+
+// Bot
+public class Bot : MonoBehaviour;
+public class BotXXXComponent : MonoBehaviour;
+
+// Player
+public class XXXXComponent : MonoBehaviour;
