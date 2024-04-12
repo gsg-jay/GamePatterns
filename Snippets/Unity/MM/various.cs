bd27e5039cb7fc6156bf6fea71b81fcea0fc8850
gsg-jay <120525462+gsg-jay@users.noreply.github.com>
2024-04-12 22:23:41 +0100

    Update various.cs

--- Snippets/Unity/MM/various.cs
+++ Snippets/Unity/MM/various.cs
@@ -1,4 +1,79 @@
-// Events
+using System;
+using UnityEngine;
+
+public class PlayableCharacter : MonoBehaviour {
+	public Player Player;
+	public bool IsDead = false;
+	protected void Update(){
+		if (IsDead){
+			HandleDead();
+			return;
+		}
+		if (IsAnimationPlaying(CharacterAnimation.TakeDamage){
+			player.SuspendControls();
+		}
+		if (IsAnimationPlayingOneOf(
+			CharacterAnimation.TakeDamageHeavy,
+			CharacterAnimation.TakeDamageLight,
+			CharacterAnimation.Die,
+			CharacterAnimation.Respawn,
+			CharacterAnimation.ThrowGroundSuplex,
+			CharacterAnimation.ThrowGroundForwardThrow,
+			CharacterAnimation.ThrowAirCatch,
+			CharacterAnimation.ThrowAirIzunaDrop,
+			CharacterAnimation.Victory,
+			CharacterAnimation.
+		){
+			player.SuspendControls();
+		} else {
+			player.EnableControls();
+		}
+	}
+	
+	public void IsCharacterAnimationOneOf(CharacterAnimation[] animations){
+		int counter = 0;
+		foreach (var anim in animations){
+			bool isPlaying = IsCharacterAnimationPlaying(anim);
+			if (isPlaying) counter++;
+			break;
+		}
+		return counter >= 1;
+	}
+
+}
+
+public class Wrestling : MonoBehaviour {
+
+	[SerializeField] PlayableCharacter _owner;
+	GameObject _victim;
+
+	public void SetVictimToThrow(GameObject victim){
+		_victim = victim;
+	}
+	public void UnsetVictimToThrow(GameObject victim){
+		_victim = null;
+	}
+	private void ParentVictimToCharacter(){
+		_victim.SetParent(_character.ThrowBox);
+	}
+	private void UnparentVictimFromCharacter(){
+		_victim.SetParent(null);
+		UnsetVictimToThrow(_victim);
+	}
+	public void HandleThrowAttempt(){
+		_owner.ChangeAnimationState(CharacterAnimation.ThrowAttempt);
+	}
+	public void HandleThrowGroundForwardThrow(){
+		_owner.ChangeAnimationState(CharacterAnimation.ThrowGroundForwardThrow);
+	}
+	public void HandleThrowAirIzunaDrop() {
+		_owner.ChangeAnimationState(CharacterAnimation.ThrowAirIzunaDrop);
+	}
+	public void HandleThrowGroundSuplex(){
+		_owner.ChangeAnimationState(CharacterAnimation.ThrowGroundSuplex);
+	}
+}
+
 
 
 // System
