e07d13e5db182cb5ae1c43d0f5ac2a6d6d45e7e7
gsg-jay <120525462+gsg-jay@users.noreply.github.com>
2024-05-12 10:48:17 +0100

    Update BotBrain.cs

--- Snippets/Unity/MM/BotBrain.cs
+++ Snippets/Unity/MM/BotBrain.cs
@@ -1,9 +1,111 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace GSG {
+  
+  public class BotBrain : MonoBehaviour {
 
-  public class BotBrain {
+    [SerializeField] Animator _animator;
 
+    private bool _isSentryMode;
+    private bool _isRecoveryMode;
+    private bool _isOffenseMode;
+    
+    public TargetDetectionEnum {
+      Close,
+      Far,
+      Null
+    };
+    
+    TargetDetectionEnum playerDetected;
+
+    [SerializeField] int _ultraGaugeCurrent;
+    [SerializeField] int _ultraGainOnTakeDamage;
+    [SerializeField] int _ultraGainOnPlayerHit;
+
+    [SerializeField] int _ultraGaugeInitial;
+    [SerializeField] int _ultraGaugeMax;
+    
+    [SerializeField] int _attackTurnsClose = 0;
+    [SerializeField] int _attackTurnsCloseMax;
+    [SerializeField] int _attackTurnsFar = 0;
+    [SerializeField] int _attackTurnsFarMax;
+    [SerializeField] float _afterPatrolWaitTime;
+
+    public AnimationClip DeathAnim;
+    public AnimationClip DefenseAnim;
+    public AnimationClip IdleAnim;
+    public AnimationClip PatrolAnim;
+    public AnimationClip AttackCloseAnim;
+    public AnimationClip AttackFarAnim;
+    public AnimationClip AttackUltraAnim;
+
+    protected void Start() {
+      StartCoroutine(Execute);
+      ultraGaugeCurrent = ultraGaugeInitial;
+    }
+
+    bool IsUltraGaugeFull() {
+      return ultraGaugeCurrent >= ultraGaugeMax;
+    }
+    
+    IEnumerator Execute() {
+      while (true){ 
+          if (_isRecoveryMode){
+            yield return StartCoroutine(Recovery());
+          }
+          if (_isSentryMode){
+            yield return StartCoroutine(Sentry());
+          }
+          if (_isOffenseMode) {
+            yield return StartCoroutine(Offense());
+          }
+          // etc..
+      }
+    }
+  
+    IEnumerator Offense() {
+      if (IsUltraGaugeFull()){
+        // ...
+      }
+    }
+    
+
+    public float Attack(string context) {
+      if (context == "ultra"){
+        _animator.Play(AttackUltra);
+        return AttackUltra.length;
+      }
+    }
+    
+    
+    // Pvt
+    private void DecrementAttackTurns(string context, int turns) {
+      if (context == "close"){
+        attackTurnsClose -= turns;
+        return;
+      }
+      if (context == "far"){
+        attackTurnsFar -= turns;
+        return;
+      }
+    }
+    private void ResetAttackTurns(string context, int turns) {
+      if (context == "close"){
+        attackTurnsClose = attackTurnsCloseMax;
+        return;
+      }
+      if (context == "far"){
+        attackTurnsFar = attackTurnsFarMax;
+        return;
+      }
+    }
+    
+    // Pub
+    public void SetPlayerDetected (TargetDetectionEnum status){
+      playerDetected = status;
+    }
+    
     public void Idle() {
 
     }
